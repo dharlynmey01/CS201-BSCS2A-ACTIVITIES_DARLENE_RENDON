0410981dde32668eb696b800ee43285a16397370
dharlynmey01 <147577503+dharlynmey01@users.noreply.github.com>
2023-11-11 20:30:19 +0800

    Create activity8.js

---
+++ activity8.js
@@ -0,0 +1,15 @@
+const favSingers = ["Arthur Nery", "Yeng Constantino", "Jessa Zaragoza"];
+console.log("My Favorite Singers: ");
+console.log(favSingers);
+
+const favNumbers = [10, 4, 5, 1];
+console.log("My Favorite Numbers:");
+console.log(favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("favSingers[0]: " + favSingers[0]);
+console.log("favSingers[1]: " + favSingers[1]);
+console.log("favSingers[2]: " + favSingers[2]);
+console.log("favNumbers[1]: " + favNumbers[1]);
+console.log("mixedArr[3]: " + mixedArr[3]);
